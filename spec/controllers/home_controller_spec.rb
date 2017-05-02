a8349f8b536867ea4035f51af509590060818c49
Willian <w.goncalves91@gmail.com>
2017-05-02 10:19:40 -0300

    Implement with authenticated user.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   context 'unauthenticated user' do
-    describe 'Get #index' do
+    describe 'GET #index' do
       before { get :index }
       it { expect(response).to redirect_to user_session_path }
+    end
+  end
+
+  context 'authenticated user' do
+    let(:user) { Fabricate :user }
+
+    before { sign_in user }
+
+    describe 'GET #index' do
+      before { get :index }
+      it { expect(response.status).to be(200) }
     end
   end
 end
