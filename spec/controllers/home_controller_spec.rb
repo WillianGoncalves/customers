3231fe49c560e29ce3145f8d44509a7354195e27
Willian <w.goncalves91@gmail.com>
2017-05-02 10:12:02 -0300

    Implement some tests.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-
+  context 'unauthenticated user' do
+    describe 'Get #index' do
+      before { get :index }
+      it { expect(response).to redirect_to user_session_path }
+    end
+  end
 end
