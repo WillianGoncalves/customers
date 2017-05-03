8b9d57d1a59dddcbf9609b73903ad997f554b76a
Willian <w.goncalves91@gmail.com>
2017-05-03 11:23:58 -0300

    Update model customer.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,14 @@
 class Customer < ApplicationRecord
-  has_one :address
-  has_many :phones
+  # inverse_of evita o erro "customer não definido" em address/phones
+  has_one :address, dependent: :destroy, inverse_of: :customer
+  has_many :phones, dependent: :destroy, inverse_of: :customer
 
   enum gender: [:male, :female]
+  
+  validates :first_name, :last_name, :avatar, :address, :phones, presence: true
 
-  validates :first_name, :last_name, :avatar, presence: true
+  # faz a validação de address e phones
+  validates_associated :address, :phones
 
   accepts_nested_attributes_for :address, :phones
 end
